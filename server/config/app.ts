6c2e2536a32eec78c8f2fe6cc82d82ed39ff52a0
Thiago Castilho <thiago.castilho@outlook.com>
2021-10-07 20:59:37 -0400

    restructure server folder

--- server/config/app.ts
+++ server/config/app.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
 
-import indexRouter from './routes/index';
+import indexRouter from '../routes/index';
 
 //Instantiate Mongo
 mongoose.connect("mongodb://localhost:27017/mobile_store");
@@ -20,15 +20,15 @@ db.once('open', function () {
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../../client')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
 
